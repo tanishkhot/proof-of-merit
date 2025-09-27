0dd48e3e5d9b4aba1c070006755e1ef6c849c806
Tanish Khot <tanishkhot@protonmail.com>
2025-09-27 08:24:14 +0530

    feat: backend setup  done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+import 'dotenv/config';
+import app from './app.js';
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
