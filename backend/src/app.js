868b49de4fc50146a393c36085d0d98d44cf0d10
Tanish Khot <tanishkhot@protonmail.com>
2025-09-27 11:01:36 +0530

    fix: added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import attestationsRouter from './routes/attestations.routes.js';
 import aisearchRouter from './routes/aisearch.routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/attestations', attestationsRouter);
